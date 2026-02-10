4322c57c0fd02395787b059c5990e674f9a33dc3
Adam <adam.sandhu123@hotmail.com>
2026-02-10 16:48:11 +0000

    completed java lab03

--- lab3-num/java/Overflow.java
+++ lab3-num/java/Overflow.java
@@ -86,7 +86,7 @@ public class Overflow
         for ( int j = 1; j <= m; j++ )
         {
             ns = ns * n;
-//             assert ?? : "Floating-point overflow";
+              assert ns != ns * 2 : "Floating-point overflow";
             resultList.add(ns);
         }
         return resultList;
@@ -112,7 +112,7 @@ public class Overflow
         for ( int j = 0; j <= m; j++ )
         {
             ns_inv = ns_inv / n; // update from 1/n^(i-1) to 1/n^i
-//             assert ?? : "Floating point underflow";
+                assert ns_inv != 0 : "Floating point underflow";
             geom_sum = ns_inv + geom_sum;
             resultList.add(geom_sum);
         }
